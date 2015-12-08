de9ea254979af72e624d292d2de651aa50024342
Nick Cadiente <nickcadiente@Nicks-MacBook-Pro.local>
2015-12-08 09:59:43 -1000

    passes all tests

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -11,21 +11,35 @@ module.exports = {
 };
 
 function _sumFibs( maxFibValue ) {
+  var firstFib= 0;
+  var secondFib= 1;
+  var nextFib= 0;
   var sum = 0;
-
   // do your work here
-
+  while (nextFib <= maxFibValue){
+    nextFib = firstFib + secondFib;
+    if (nextFib%2 === 0){
+      sum+= nextFib;
+    }
+    firstFib = secondFib;
+    secondFib = nextFib;
+  }
   return sum;
 }
 
 // bonus round
-function highestFibonacciNumber (maxFibValue){
+function _highestFibonacciNumber (maxFibValue){
+  var firstFib= 0;
+  var secondFib= 1;
+  var nextFib = 0;
   var highest = 0;
-
   //define your base case, validate your input
-
-
+  while (nextFib < maxFibValue){
+    nextFib = firstFib + secondFib;
+    firstFib = secondFib;
+    secondFib = nextFib;
+  }
+  highest = firstFib;
   //do your work here
-
   return highest;
-};
+}
